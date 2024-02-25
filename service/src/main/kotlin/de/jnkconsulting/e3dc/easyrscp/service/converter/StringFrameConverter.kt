a5e41bfe179bc55bc81622d036abdc2ea9a98d07
Jochen Zink <jochen.zink@jnk-consulting.de>
2024-02-25 15:31:39 +0100

    #38 Added omit credentials option to StringFrameConverter

--- service/src/main/kotlin/de/jnkconsulting/e3dc/easyrscp/service/converter/StringFrameConverter.kt
+++ service/src/main/kotlin/de/jnkconsulting/e3dc/easyrscp/service/converter/StringFrameConverter.kt
@@ -5,14 +5,21 @@ import de.jnkconsulting.e3dc.easyrscp.api.frame.Data
 import de.jnkconsulting.e3dc.easyrscp.api.frame.DataParser
 import de.jnkconsulting.e3dc.easyrscp.api.frame.DataType
 import de.jnkconsulting.e3dc.easyrscp.api.frame.Frame
+import de.jnkconsulting.e3dc.easyrscp.api.frame.tags.RSCPTag
 
 /**
  * Simple implementation of the [FrameConverter] interface to convert a complete frame into a string. The individual data fields are output per line.
  * Container data indented accordingly.
  *
+ * @param omitAuthenticationCredentials If true (default), the values of the tags of Type [de.jnkconsulting.e3dc.easyrscp.api.frame.tags.RSCPTag.AUTHENTICATION_USER], [de.jnkconsulting.e3dc.easyrscp.api.frame.tags.RSCPTag.AUTHENTICATION_PASSWORD] and [de.jnkconsulting.e3dc.easyrscp.api.frame.tags.RSCPTag.REQ_SET_ENCRYPTION_PASSPHRASE] are masked with three ***. If set to false, the original value is shown.
+ *
  * @since 2.0
+ *
+ * @since 2.3.1
+ * - added parameter omitAuthenticationCredentials
  */
-class StringFrameConverter: FrameConverter<String> {
+class StringFrameConverter(
+    private val omitAuthenticationCredentials: Boolean = true): FrameConverter<String> {
 
     override fun invoke(frame: Frame): String =
         StringBuilder()
@@ -34,8 +41,24 @@ class StringFrameConverter: FrameConverter<String> {
                 .forEach { result.append(convertData(it, level + 1, parser)).append("\n") }
         }
         else {
-            result.append("${tabs}Value:\t${toPrint.valueAsString()} - ${toPrint.value.toHexString()}\n")
+            result.append("${tabs}Value:\t")
+            if (shouldMask(toPrint)) {
+                result.append("*** - ***\n")
+            }
+            else {
+                result.append("${toPrint.valueAsString()} - ${toPrint.value.toHexString()}\n")
+            }
         }
         return result.toString()
+    }
+
+    private fun shouldMask(toPrint: Data) =
+        omitAuthenticationCredentials && TAGS_TO_OMIT.contains(toPrint.tagObject())
+
+    companion object {
+        val TAGS_TO_OMIT = setOf(
+            RSCPTag.AUTHENTICATION_USER,
+            RSCPTag.AUTHENTICATION_PASSWORD,
+            RSCPTag.REQ_SET_ENCRYPTION_PASSPHRASE)
     }
 }
